c6e3495b92ac2629622d488d0414da9654359b6d
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-07 09:37:44 +0530

    Create start.py

---
+++ ForceTG_Aerobot/start.py
@@ -0,0 +1,14 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+from Config import START_IMG
+
+# Start Message
+@Client.on_message(filters.private & filters.command("start"))
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user.mention
+	await msg.reply_photo(START_IMG,
+		caption=Data.START.format(msg.from_user.mention, mention),
+		reply_markup=InlineKeyboardMarkup(Data.buttons)
+                       )
