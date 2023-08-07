eedd25640800e07dd9f6a4c8042318554b9bebfd
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-07 12:38:35 +0530

    Update help.py

--- ForceTG_Aerobot/help.py
+++ ForceTG_Aerobot/help.py
@@ -4,7 +4,10 @@ from pyrogram.types import InlineKeyboardMarkup
 from Config import START_IMG
 
 # Help Message
-@Client.on_message(filters.private & filters.command("help"))
+@Client.on_message(filters.private & filters.incoming & filters.command("help"))
+@Client.on_message(filters.text & filters.incoming & filters.command("help@Aero_Force2_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("help@Aero_Force_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("help"))
 async def _help(bot, msg):
     await msg.reply_photo(
         START_IMG,
