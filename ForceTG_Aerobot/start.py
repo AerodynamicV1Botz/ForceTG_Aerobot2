665204e9b07d8fd0a0625f4f540ef32c4d2a6f53
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-07 12:41:16 +0530

    Update start.py

--- ForceTG_Aerobot/start.py
+++ ForceTG_Aerobot/start.py
@@ -4,8 +4,10 @@ from pyrogram.types import InlineKeyboardMarkup
 from Config import START_IMG
 
 # Start Message
-@Client.on_message(filters.private & filters.command("start"))
-async def start(bot, msg):
+@Client.on_message(filters.private & filters.text & filters.incoming & filters.command("start"))
+@Client.on_message(filters.text & filters.incoming & filters.command("start@Aero_Force2_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("start@Aero_Force_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("start"))async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user.mention
 	await msg.reply_photo(START_IMG,
