69c3b777607091a0cfc39e6d36c6c1e8de47ba48
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-08 11:55:09 +0530

    Update id.py

--- ForceTG_Aerobot/id.py
+++ ForceTG_Aerobot/id.py
@@ -5,4 +5,4 @@ from pyrogram import Client, filters
 @Client.on_message(filters.text & filters.incoming & filters.command("id@Aero_Force2_Subscriber_Bot"))
 @Client.on_message(filters.text & filters.incoming & filters.command("id@Aero_Force_Subscriber_Bot"))
 async def id(_, msg):
-    await msg.reply(f"ᴄʜᴀᴛ ɪᴅ ɪs : `{msg.chat.id}`", quote=True)
+    await msg.reply(f"Chat ID is : `{msg.chat.id}`", quote=True)
