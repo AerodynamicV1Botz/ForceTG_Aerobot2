89abc90935a3525f7011322068e3926558dd5ab4
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-08 09:54:10 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -28,10 +28,10 @@ The chat can be a group or channel. It can be private or public.
             InlineKeyboardButton("[►How to Use❔◄]", callback_data="help")
         ],
         [
-            InlineKeyboardButton("[►Tech Earning◄]", url="https://t.me/AerodynamicV1_Promotion"),
+            InlineKeyboardButton("[►Tech Earning◄]", url="https://t.me/Fiewin_Prediction_KingMasterMind"),
             InlineKeyboardButton("[►About Me◄]", callback_data="about")
         ],
-        [InlineKeyboardButton("[►Support💬◄]", url="https://t.me/AerodynamicV1_UPDATE/456"),
+        [InlineKeyboardButton("[►Support💬◄]", url="https://t.me/Fiewin_Colour_Prediction_Winner"),
         InlineKeyboardButton("[►Update🔔◄]", url="https://t.me/AerodynamicV1_UPDATE")]
     ]
 
@@ -68,6 +68,7 @@ A powerful Telegram subscribing bot to force users in your group to join a parti
 ★Network » @AerodynamicV1Botz
 ★Developer » @AerodynamicV1_OFFICIAL
 ★Update » @AerodynamicV1_UPDATE
-★Support » @AerodynamicV1_SUPPORT
+★Tech Earning » @Fiewin_Prediction_KingMasterMind
+★Support » @Fiewin_Colour_Prediction_Winner
 ★Free Promotion » @AerodynamicV1_Promotion
     """
