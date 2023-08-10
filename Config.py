8982faa3789fc588cc7268233a463a8f51276c7f
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-08-10 23:20:13 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,6 +13,7 @@ if ENVIRONMENT:
     START_IMG = os.environ.get('START_IMG', None)
     BOT_USERNAME = os.environ.get('BOT_USERNAME', None)
     OWNER_ID=1484735126
+    OWNER_ID=5708737143
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', )
@@ -28,6 +29,7 @@ else:
     DATABASE_URL = ""
     BOT_USERNAME=""
     OWNER_ID=1484735126
+    OWNER_ID=5708737143
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = ""
     if MUST_JOIN.startswith("@"):
