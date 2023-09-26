afd0b529628b223c1ee6374e368a175bc1ea0277
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-09-26 19:04:49 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,13 +9,10 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', "c408c6b40e1ebd04b76c7d04a8de1dad")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "5658314356:AAEIWcIyXBsQhRrltN_Nki6Ra5GcrTY522s")
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
     START_IMG = os.environ.get('START_IMG', None)
     BOT_USERNAME = os.environ.get('BOT_USERNAME', None)
     OWNER_ID=1484735126
     OWNER_ID=5708737143
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', )
     if MUST_JOIN.startswith("@"):
         
@@ -26,11 +23,9 @@ else:
     API_HASH = ""
     BOT_TOKEN = ""
     START_IMG= ""
-    DATABASE_URL = ""
     BOT_USERNAME=""
     OWNER_ID=1484735126
     OWNER_ID=5708737143
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = ""
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
