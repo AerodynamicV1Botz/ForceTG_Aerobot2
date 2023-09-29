74e1acde94465c04bd3a41445191cda8de83f22d
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-09-29 20:04:50 +0530

    Update fsubbot.py

--- fsubbot.py
+++ fsubbot.py
@@ -25,7 +25,7 @@ app = Client(
 # Run Bot
 if __name__ == "__main__":
     try:
-         app = web.AppRunner(await web_server())
+        app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
