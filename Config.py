faf7a52afb9dab9f7857edfa53a3c351e76d4d84
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-09-29 09:13:58 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,10 +10,13 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', "c408c6b40e1ebd04b76c7d04a8de1dad")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "5219214161:AAFudzJ_ixUrexsdEUPFNWxynDahgm7P96g")
     START_IMG = os.environ.get('START_IMG', "https://te.legra.ph/file/023e6daf966ba99871b80.jpg")
-    BOT_USERNAME = os.environ.get('BOT_USERNAME', "@Aero_Force_Subscriber_Bot")
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    BOT_USERNAME = os.environ.get('BOT_USERNAME', None)
     OWNER_ID=1484735126
     OWNER_ID=5708737143
-    MUST_JOIN = os.environ.get('MUST_JOIN', )
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    MUST_JOIN = os.environ.get('MUST_JOIN', "AerodynamicV1_Update")
     if MUST_JOIN.startswith("@"):
         
         MUST_JOIN = MUST_JOIN.replace("@", "")
@@ -23,9 +26,11 @@ else:
     API_HASH = ""
     BOT_TOKEN = ""
     START_IMG= ""
+    DATABASE_URL = ""
     BOT_USERNAME=""
     OWNER_ID=1484735126
     OWNER_ID=5708737143
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = ""
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
