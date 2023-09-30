3173ffde21cf193fde5e822b3dedcf8eaad57dcc
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-09-30 10:17:37 +0530

    Update fsubbot.py

--- fsubbot.py
+++ fsubbot.py
@@ -2,9 +2,8 @@ import Config
 import logging
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
-from pyrogram import types
 from aiohttp import web
-from ForceTG_Aerobot import web_server
+from ForceTG_Aerobot2.0 import web_server
 
 PORT = "8080"
 
