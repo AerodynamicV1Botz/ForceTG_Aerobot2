55fe8839b30b25af31360eab0eeea35b2c5b2672
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-09-30 11:05:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AerodynamicV1Botz/ForceTG_Aerobot2.0.git /ForceTG_Aerobot2.0
+  git clone https://github.com/AerodynamicV1Botz/ForceTG_Aerobot2.git /ForceTG_Aerobot2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ForceTG_Aerobot2.0
+  git clone $UPSTREAM_REPO /ForceTG_Aerobot2
 fi
-cd /ForceTG_Aerobot2.0
+cd /ForceTG_Aerobot2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 fsubbot.py
